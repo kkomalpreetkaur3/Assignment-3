d67e5d79e46c3725d69da70f47ae15ff839c1f4a
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-03-23 22:13:21 -0500

    feat: integrate CORS middleware into express app

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@ import express from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 import { getHelmetConfig } from "../config/helmetConfig";
-
+import { getCorsOptions } from "../config/corsConfig";
 const app = express();
 
 app.use(express.json());
 app.use(getHelmetConfig());
+app.use(cors(getCorsOptions()));
 
 app.use("/api/v1/health", healthRoutes);
 app.use("/api/v1/events", eventRoutes);
