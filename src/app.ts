86d8349568f9f29bd5974e9b4da79ecf8352900c
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-03-23 22:15:22 -0500

    feat: install swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ dotenv.config();
 
 import { getHelmetConfig } from "../config/helmetConfig";
 import { getCorsOptions } from "../config/corsConfig";
+
 const app = express();
 
 app.use(express.json());
